f3e4ee32396ef2070e3816105027fd1ffa3fc0b7
ssa_book <3092040@mail.ru>
2016-11-11 17:30:19 +0500

    Рабочая версия программы. Необходимый минимум.

--- GameWindow.java
+++ GameWindow.java
@@ -1,14 +1,38 @@
 package ru.geekbrains.catch_the_drops;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
 
 public class GameWindow extends JFrame{
 
     private static GameWindow game_window;
+    private static Image background;
+    private static Image drop;
+    private static Image game_over;
+    private static float drop_left = 100;
+    private static float drop_top = -100;
+    private static long last_frame_time;
+    private static float drop_v=100;
+    private static int score=0;
 
-    public static void main(String[] args) {
-	// write your code here
+// Дополнительные поля для ограничения игры
+    private static int game_range;
+
+
+    public static void main(String[] args) throws IOException {
+	    background = ImageIO.read(GameWindow.class.getResourceAsStream("background.png"));
+        drop = ImageIO.read(GameWindow.class.getResourceAsStream("drop.png"));
+        game_over = ImageIO.read(GameWindow.class.getResourceAsStream("game_over.png"));
+        last_frame_time = System.nanoTime();
+
+//todo добавить меню игры
+
+//todo добавить уровеь сложности
+
         game_window=new GameWindow();
         game_window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         game_window.setLocation(200,100);
@@ -16,16 +40,40 @@ public class GameWindow extends JFrame{
         game_window.setResizable(false);
 
         GameField game_field = new GameField();
+        game_field.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                int x = e.getX();
+                int y = e.getY();
+                float drop_right = drop_left + drop.getWidth(null);
+                float drop_bottom = drop_top + drop.getHeight(null);
+                boolean in_drop = x>=drop_left && x<=drop_right && y>=drop_top && y<=drop_bottom;
+                if (in_drop){
+                    drop_top=-100;
+                    drop_left = (int) (Math.random()*(game_window.getWidth() - drop.getWidth(null)));
+                    drop_v+=10;
+                    score++;
+                    game_window.setTitle("Catche a drop. Your score: "+ score);
+                }
+            }
+        });
+
+
 
         game_window.add(game_field);
         game_window.setVisible(true);
-        game_window.setTitle("Поймай каплю");
+        game_window.setTitle("Catche a drop. Your score: "+ score);
 
     }
     private static void onRepaint(Graphics g){
-        g.fillOval(50,50,200,100);
-        g.fillOval(10,10,200,100);
-        g.drawLine(10, 10, 50, 50);
+        long current_time = System.nanoTime();
+        float delta_time = (current_time-last_frame_time)*0.000000001f;
+        last_frame_time=current_time;
+        drop_top=drop_top+drop_v*delta_time;
+
+        g.drawImage(background,0,0,null);
+        g.drawImage(drop, (int) drop_left, (int) drop_top, null);
+        if (drop_top>=game_window.getHeight()) { g.drawImage(game_over,280,100,null); }
     }
 
     private static class GameField extends JPanel{
@@ -33,6 +81,7 @@ public class GameWindow extends JFrame{
         protected void paintComponent(Graphics g){
             super.paintComponent(g);
             onRepaint(g);
+            repaint();
         }
     }
 }
